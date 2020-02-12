395c28457b268c6fdbfd18f53e289bb530639c53
John Rooey <JROOEY@mail.depaul.edu>
2020-02-11 21:34:04 -0600

    problem 2 a,b,c done

--- problem2.py
+++ problem2.py
@@ -1,26 +1,75 @@
 import statistics
 from pathlib import Path
 
-def mean():
-    cat = input('Pick a category: ')
+def makeLst():
+    """Makes list of values out of avocado.csv"""
     folder = Path('data/')
     file = folder / 'avocado.csv'
     infile = open(file, 'r')
-    raw = infile.read(5000)
+    raw = infile.read()
     infile.close()
     rawLst = raw.split(',')
+    return rawLst
 
+def readAndComputeMean_SM():
+    """Computes mean of selected variable in avocado.csv"""
+    rawLst = makeLst()
+    cat = input('Pick a category: ')
+
+    dict = {'Average Price': 2, 'Total Volume': 3, 'Total Bags': 7, 'Small Bags': 8, 'Large Bags': 9, 'XLarge Bags': 10}
+
+    # Creates list of str numbers for given variable
     strVolLst = []
-    count = 3
-    while count < 100:
+    count = dict[cat]
+    while count < len(rawLst):
         strVolLst.append(rawLst[count])
         count += 13
 
+    # Creates list of int numbers for given variable
     numVolLst = []
-    count2 = 1
-    while count2 < len(strVolLst):
-        numVolLst.append(float(strVolLst[count2]))
-        count += 1
-    print(numVolLst)
+    for i in range(1, len(strVolLst)):
+        numVolLst.append(float(strVolLst[i]))
 
-mean()
+    return round(statistics.mean(numVolLst), 2)
+
+def readAndComputeSD_SM():
+    """Computes standard deviation of selected variable in avocado.csv"""
+    rawLst = makeLst()
+    cat = input('Pick a category: ')
+
+    dict = {'Average Price': 2, 'Total Volume': 3, 'Total Bags': 7, 'Small Bags': 8, 'Large Bags': 9, 'XLarge Bags': 10}
+
+    # Creates list of str numbers for given variable
+    strVolLst = []
+    count = dict[cat]
+    while count < len(rawLst):
+        strVolLst.append(rawLst[count])
+        count += 13
+
+    # Creates list of int numbers for given variable
+    numVolLst = []
+    for i in range(1, len(strVolLst)):
+        numVolLst.append(float(strVolLst[i]))
+
+    return round(statistics.stdev(numVolLst), 2)
+
+def readAndComputeMedian_SM():
+    """Computes median of selected variable in avocado.csv"""
+    rawLst = makeLst()
+    cat = input('Pick a category: ')
+
+    dict = {'Average Price': 2, 'Total Volume': 3, 'Total Bags': 7, 'Small Bags': 8, 'Large Bags': 9, 'XLarge Bags': 10}
+
+    # Creates list of str numbers for given variable
+    strVolLst = []
+    count = dict[cat]
+    while count < len(rawLst):
+        strVolLst.append(rawLst[count])
+        count += 13
+
+    # Creates list of int numbers for given variable
+    numVolLst = []
+    for i in range(1, len(strVolLst)):
+        numVolLst.append(float(strVolLst[i]))
+
+    return round(statistics.median(numVolLst), 2)

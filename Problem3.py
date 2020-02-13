b80bde0a18fb9d3c604894654eee2fb7290746b2
John Rooey <JROOEY@mail.depaul.edu>
2020-02-13 11:30:15 -0600

    finished problem 3

--- Problem3.py
+++ Problem3.py
@@ -1,12 +1,20 @@
 import random
 
 def game():
+    """Dice game, lots of skill required"""
     balance = 100
+    print('Your balance is $100')
+    print('Place bet of 0 to walk away')
     while balance > 0:
-        try:
-            bet = eval(input('Place a bet: '))
-        except:
-            print('numbers only please')
+        while True:
+            try:
+                bet = eval(input('Place a bet: '))
+                if bet == 0:
+                    print('You walked away with ${}'.format(balance))
+                    return
+                break
+            except:
+                print('Numbers Only Please')
         while bet > balance or bet < 0:
             print('Try Again')
             bet = eval(input('Place a bet: '))
@@ -16,9 +24,10 @@ def game():
             score = a + b
             if score == 7 or score == 12:
                 balance += bet * 2
-                print(a, b, balance, bet)
+                print('Die 1: {} Die 2: {}'.format(a, b))
+                print('Your balance is ${} after winning ${}'.format(balance, bet * 2))
             else:
-                print(a, b, balance, bet)
+                print('Die 1: {} Die 2: {}'.format(a, b))
                 yorn = input('Double or nothing? Y/N: ')
                 while yorn.lower() not in ['yes', 'y', 'no', 'n']:
                     yorn = input('Double or nothing? Y/N: ')
@@ -27,13 +36,17 @@ def game():
                         c = random.randrange(1, 7)
                         if score + c == 7 or score + c == 12:
                             balance += bet * 3
-                            print(a, b, c, balance, bet)
+                            print('Die 1: {} Die 2: {} Die 3: {}'.format(a, b, c))
+                            print('Your balance is ${} after winning ${}'.format(balance, bet * 3))
                         else:
                             balance -= bet * 2
-                            print(a, b, c, balance, bet)
+                            print('Die 1: {} Die 2: {} Die 3: {}'.format(a, b, c))
+                            print('Your balance is ${} after losing ${}'.format(balance, bet * 2))
                     else:
                         balance -= bet
-                        print(balance, bet)
+                        print('Your balance is ${} after losing ${}'.format(balance, bet))
     else:
         print('You lost all your money :(')
+    return
+
 game()
